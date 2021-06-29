4dd29f2d6701d4319e21971d4ff5580086a09ade
Josep Suarez <djose.anton@gmail.com>
2021-06-29 12:29:39 +0200

    Add some styles to Header component

--- my-favorite-movies/src/components/Favourites/index.jsx
+++ my-favorite-movies/src/components/Favourites/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import List from '../Movies/List';
+import './favourites.css';
 
 const Favourites = () => {
   const favourites = useSelector((store) => store.favourites);
   return (
-    <>
-      <h3>My Favourites</h3>
+    <section className="favourites">
+      <h3 className="favourites__title">My Favourites</h3>
       <List movies={favourites} />
-    </>
+    </section>
   );
 };
 
